599a5b95b465033351138188ec28d8eb4a327ff0
AgomohC <chinaemerem98@yahoo.com>
2021-10-15 01:21:23 +0100

    fixed some bugs

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(helmet());
-
 app.use("/api", libraryRoutes);
 app.use(notFound);
 app.use(errorHandler);
